a3818ab0e1295bfe85d228dbc46c877b3d7eef5b
Jesutoni Odufeso <ojesutoni@gmail.com>
2026-05-08 17:29:41 +0100

    initial commit

---
+++ frontend/vite.config.js
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite'
+import react, { reactCompilerPreset } from '@vitejs/plugin-react'
+import babel from '@rolldown/plugin-babel'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    babel({ presets: [reactCompilerPreset()] })
+  ],
+  server: {
+    port: 5000,
+    host: true,
+    open: true,
+    cors: true,
+    proxy: {
+      '/api': 'http://localhost:3000',
+    },
+  },
+})
